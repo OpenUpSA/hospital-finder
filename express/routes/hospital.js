06daed69a939bbb8f114874d21b5e24513ae1647
Jason Norwood-Young <jason@10layer.com>
2014-10-15 11:55:45 +0200

    Feedback form closes #10

--- express/routes/hospital.js
+++ express/routes/hospital.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 var request = require("request");
 var connection = require("./connection");
+var multer  = require('multer')
+var fs = require('fs');
+
+router.use(multer({ dest: './uploads/'}));
 
 router.get('/:hospital_id', function(req, res) {
 	var uid = req.params.hospital_id;
@@ -15,7 +19,28 @@ router.get('/:hospital_id', function(req, res) {
 		var hospital = rows[0];
 		res.render("hospital", { hospital: hospital, title: hospital.name });
 	});
+});
 
+router.post('/:hospital_id', function(req, res) {
+	console.log(req.body);
+	console.log(req.files);
+	var photo = "";
+	var photo_originalname = "";
+	if (req.files.photo) {
+		photo = req.files.photo.name;
+		photo_originalname = req.files.photo.originalname;
+	}
+	connection.query("INSERT INTO hospitals.feedback (hospital_uid, ip, rating, comments, can_contact, contact_details, contact_name, photo, photo_originalname) VALUES (" +
+		connection.escape(req.params.hospital_id) + ", " + 
+		connection.escape(req.connection.remoteAddress) + ", " + 
+		connection.escape(req.body.rating) + ", " + 
+		connection.escape((req.body.comments)? req.body.comments : "" ) + ", " + 
+		connection.escape((req.body.can_contact)? 1 : 0) + ", " + 
+		connection.escape((req.body.contact_details)? req.body.contact_details : "" ) + ", " + 
+		connection.escape((req.body.contact_name)? req.body.contact_name : "") + ", " + 
+		connection.escape(photo) + ", " + 
+		connection.escape(photo_originalname) + ")");
+	res.render("thanks", { title: "Thanks for your submission" });
 });
 
 module.exports = router;
