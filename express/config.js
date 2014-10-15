283d77ccfa69b6b87419f7f4b5401237eb6d7ae8
Jason Norwood-Young <jason@10layer.com>
2014-10-15 14:16:29 +0200

    Config file

---
+++ express/config.js
@@ -0,0 +1,10 @@
+var config = {
+	google_email: "blah@gmail.com",
+	google_pass: "blahblah",
+	email_from: "no-reply@code4sa.org",
+	email_host: "smtp.gmail.com",
+	email_user: "blah@gmail.com",
+	email_pass: "blahblah"
+}
+
+module.exports = config;
