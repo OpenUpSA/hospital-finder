31355230e3ba8621b242b9b4daa870d2a6d5d5b8
Jason Norwood-Young <jason@10layer.com>
2014-10-16 13:14:19 +0200

    Add some more properties to the config file

--- express/config.js
+++ express/config.js
@@ -1,10 +1,12 @@
 var config = {
-	google_email: "blah@gmail.com",
-	google_pass: "blahblah",
-	email_from: "no-reply@code4sa.org",
+	google_email: "me@gmail.com",
+	google_pass: "MyPassword",
+	email_from: "Code4SA <jason@code4sa.org>",
 	email_host: "smtp.gmail.com",
-	email_user: "blah@gmail.com",
-	email_pass: "blahblah"
+	email_user: "me@gmail.com",
+	email_pass: "MyPassword",
+	email_to: "blah@blah.com, blahblah@blah.com",
+	email_subject: 'New Submission from Hospitals App',
 }
 
 module.exports = config;
