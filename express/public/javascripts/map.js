118b6b7ea86549552d1ebc932c087ad431f088aa
Jason Norwood-Young <jason@10layer.com>
2014-10-16 17:21:58 +0200

    Show multiple hospitals

--- express/public/javascripts/map.js
+++ express/public/javascripts/map.js
@@ -1,4 +1,16 @@
 
+function stars(perc) { // Fuck DRY
+  var count = Math.round(perc/20);
+  var s = "";
+  for (var i = 0; i < count; i++) {
+    s += "<i class='fa fa-star'></i>";
+  }
+  for (var i = count; i < 5; i++) {
+    s += "<i class='fa fa-star-o'></i>";
+  }
+  return s;
+}
+
 function initialize() {
   if (hospital) {
     var isDraggable = $(document).width() > 480 ? true : false;
@@ -10,24 +22,25 @@ function initialize() {
       scrollwheel: false,
     }
     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-    var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      title: hospital.name
-    });
-    var count = Math.round(hospital.rating/20);
-    var s = "";
-    for (var i = 0; i < count; i++) {
-      s += "<i class='fa fa-star'></i>";
-    }
-    for (var i = count; i < 5; i++) {
-      s += "<i class='fa fa-star-o'></i>";
-    }
-    var infowindow = new google.maps.InfoWindow({
-      content: "<h3>" + hospital.name + "</h3><h4>" + s + "</h4>"
-    });
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.open(map,marker);
+    var points = [];
+    
+    $.get("/find", {lat: hospital.latitude, lng: hospital.longitude }, function(data) {
+      $.each(data.result, function(id, h) {
+        var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(h.latitude, h.longitude),
+          map: map,
+          title: h.name
+        });
+        var infowindow = new google.maps.InfoWindow({
+          content: "<h3><a href='/hospital/" + h.uid + "'>" + h.name + "</a></h3><h4>" + stars(h.overall_performance) + "</h4>"
+        });
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(map, marker);
+        });
+        points.push({ marker: marker, infowindow: infowindow });
+      });
+      var first_point = points[0];
+      first_point.infowindow.open(map, first_point.marker); // This makes the primary hospital's infopage pop up
     });
   }
 
